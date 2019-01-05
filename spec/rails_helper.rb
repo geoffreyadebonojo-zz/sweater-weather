d09ded8c68607efa0acbba77223061a347888e26
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-05 15:57:43 -0700

    setup simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,6 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -30,6 +29,9 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+SimpleCov.start "rails"
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
