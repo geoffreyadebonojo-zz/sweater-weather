6db2f122b724c2ae7a836eb54ff54865e7640c2e
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-05 10:32:18 -0700

    add hourly and daily forecasts

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,36 +1,56 @@
-require 'securerandom'
-
 class Forecast
 
   def initialize(data, params)
     @id = data.json[:daily][:data][0][:time]
-    @date = Time.at(@id).to_datetime
-    @city = params[:location].split(",").first.capitalize
-    @state = params[:location].split(",").last.upcase
 
     currently = data.json[:currently]
     hourly = data.json[:hourly]
     daily = data.json[:daily]
     today = daily[:data][0]
     tomorrow = daily[:data][1]
+    long = data.json[:longitude]
+    lat = data.json[:latitude]
 
+    # Upper Left Box
     @summary = daily[:summary]
-    @today_summary = today[:summary]
-    @tonight_summary = tomorrow[:summary]
-
-    @long = data.json[:longitude]
-    @lat = data.json[:latitude]
-
+    @temperature = daily[:temperature]
     @high = today[:temperatureHigh]
     @low = today[:temperatureLow]
+    @city = params[:location].split(",").first.capitalize
+    @state = params[:location].split(",").last.upcase
+    @date = Time.at(@id).to_datetime
 
-    @feels_like_high = today[:apparentTemperatureHigh]
-    @feels_like_low = today[:apparentTemperatureLow]
-
+    # Upper Right Box
+    @today_summary = today[:summary]
+    @tonight_summary = tomorrow[:summary]
+    @feels_like = data.json[:currently][:apparentTemperature]
     @humidity = today[:humidity]
     @visibility = today[:visibility]
     @uv_index = today[:uvIndex]
 
+    # Main Box
+    @hourly_forecasts = data.json[:hourly][:data].map do |hourly_forecast|
+      forecast = {
+        date: Time.at(hourly_forecast[:time]).to_date,
+        time: Time.at(hourly_forecast[:time]).to_s.split("T").last.split(".").first,
+        icon: hourly_forecast[:icon],
+        temp: hourly_forecast[:temperature]
+      }
+      # HourlyForecast.new(forecast)
+    end
+
+    @daily_forecasts = data.json[:daily][:data].map do |daily_forecast|
+      {
+        sunrise: Time.at(daily_forecast[:sunriseTime]),
+        sunset: Time.at(daily_forecast[:sunsetTime]),
+        precipitation: daily_forecast[:precipProbability] * 100,
+        icon: daily_forecast[:icon],
+        high: daily_forecast[:temperatureHigh],
+        low: daily_forecast[:temperatureLow]
+      }
+      # DailyForecast.new(forecast)
+    end
+    binding.pry
   end
 
 end
