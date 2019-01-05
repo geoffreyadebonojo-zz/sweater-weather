4b8bf4ba33de5caa0ede75f1dffac05ffc10eb7b
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-05 15:56:25 -0700

    add Json parse to user login test

--- spec/requests/api/v1/user_login_spec.rb
+++ spec/requests/api/v1/user_login_spec.rb
@@ -2,32 +2,35 @@ require 'rails_helper'
 
 RSpec.describe "User login" do
   it "API recieves post request" do
-    
+
     data = {
     "email": "awesomesauce@gmail.com",
     "password": "abc123doremi",
     "password_confirmation": "abc123doremi"
-    }    
+    }
 
     post "/api/v1/users", params: data
 
     expect(response).to be_successful
-    expect(response.body).to eq("{\"message\":\"Sucessfully created! Here's your key\",\"key\":\"abc123doreimi\"}")
+    body = JSON.parse(response.body)
+    expect(body["key"]).to eq("thisIsYourApiKey.UR#WLCERZ")
+    expect(body["message"]).to eq("Sucessfully created! Here's your key")
     expect(User.last.email).to eq("awesomesauce@gmail.com")
   end
 
   it "API rejects post request with wrong password" do
-    
+
     data = {
     "email": "awesomesauce@gmail.com",
     "password": "abc123doremi",
     "password_confirmation": "youandme"
-    }    
+    }
 
     post "/api/v1/users", params: data
 
     expect(response.status).to eq(400)
-    expect(response.body).to eq("{\"data\":\"problem occured!\"}")
+    body = JSON.parse(response.body)
+    expect(body["message"]).to eq("problem occured!")
     expect(User.count).to eq(0)
   end
 end
