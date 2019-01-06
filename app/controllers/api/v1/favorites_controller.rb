2400811b1e399347df6218f61dbf57f9b9e9db9a
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-06 09:47:14 -0700

    add index method

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,11 +2,16 @@ class Api::V1::FavoritesController < ApplicationController
   def create
     user = User.find_by(api_key: params[:api_key])
     if user
-      user.favorites.create!(location: params[:locatioon])
+      user.favorites.create!(location: params[:location])
     else
       render json: {
         message: "something went wrong!"
       }, status: 401
     end
+  end
+
+  def index
+    user = User.find_by(api_key: params[:api_key])
+    render json: user.favorites
   end
 end
