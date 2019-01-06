94d7ba10ac682c9b9d321b5b533f4a9561b00243
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-06 15:57:26 -0700

    set up forecast facade with forecast service call

--- app/facades/forecasts_facade.rb
+++ app/facades/forecasts_facade.rb
@@ -1,11 +1,8 @@
 class ForecastsFacade
 
   def create_forecast(data, params)
-    results = data[:results][0][:geometry][:location]
-    lat = results[:lat]
-    long = results[:lng]
-
-    Forecast.new(ForecastService.new(lat, long), params)
+    forecast = ForecastService.new(data)
+    Forecast.new(forecast.json, params)
   end
 
 end
