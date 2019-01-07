38a1d1f2e6a71d5ffe5839fb9f5986aa936ef137
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-06 19:40:45 -0700

    VCR config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,25 +4,26 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
+require 'vcr'
+require 'webmock/rspec'
+require 'simplecov'
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+WebMock.disable_net_connect!(allow: ['http://localhost:3000/'])
 
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
+VCR.configure do |config|
+
+  config.ignore_localhost = true
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock, :faraday
+  config.configure_rspec_metadata!
+
+  config.filter_sensitive_data("<GOOGLE_GEOCODE_API>") { ENV['GOOGLE_GEOCODE_API'] }
+  config.filter_sensitive_data("<DARK_SKIES_API>") { ENV['DARK_SKIES_API'] }
+
+  config.allow_http_connections_when_no_cassette = true
+end
+
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
