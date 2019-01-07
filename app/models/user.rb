3b9a5c5b580a6a9f5e562bafb15a3e6cf5f6d45b
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-06 21:05:06 -0700

    create favorite forecasts method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ApplicationRecord
 
   has_many :favorites
 
+  def favorites_forecasts
+    locations = self.favorites.pluck(:location)
+    forecast = Hash.new
+    locations.map do |location|
+      forecast[location] = GeocodeService.new(location).lat_lng
+    end
+  end
+
   private
   def get_api_key
     self.api_key = SecureRandom.uuid
