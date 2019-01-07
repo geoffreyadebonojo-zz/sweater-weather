7d013aa74d56cb77656e817b0952c4b1ddd423c6
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-06 19:40:08 -0700

    update to use VCR

--- spec/services/geocode_service_spec.rb
+++ spec/services/geocode_service_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe GeocodeService do
   it "Gets JSON Data" do
-    service = GeocodeService.new("Dallas, TX")
-    expect(service).to be_an_instance_of(GeocodeService)
-    expect(service.lat_lng).to be_a(Hash)
+    VCR.use_cassette("vcr_geocode_results") do
+      service = GeocodeService.new("Dallas, TX")
+      expect(service).to be_an_instance_of(GeocodeService)
+      expect(service.lat_lng).to be_a(Hash)
+    end
   end
-
 end
