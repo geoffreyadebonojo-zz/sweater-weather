b5ecefde078b6fa598356bc5eac6494104945603
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-07 14:31:29 -0700

    add test for deleting locations that aren't in favorites

--- spec/requests/api/v1/favorite_deletion_spec.rb
+++ spec/requests/api/v1/favorite_deletion_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe "User DELETES a favorite" do
 
     delete "/api/v1/favorites", params: user_api_key
     expect(response.status).to eq(200)
-    body = JSON.parse(response.body)
+    body = JSON.parse(response.body)["data"]
     expect(body.count).to eq(1)
     expect(body.first["location"]).to eq("Dallas, TX")
 
@@ -39,6 +39,27 @@ RSpec.describe "User DELETES a favorite" do
 
     delete "/api/v1/favorites", params: user_api_key
     expect(response.status).to eq(401)
+    body = JSON.parse(response.body)
+    expect(user.favorites.count).to eq(2)
+
+  end
+
+  it "and enters a non-existent location" do
+    user_data = {
+    "email": "awesomesauce@gmail.com",
+    "password": "abc123doremi"
+    }
+    user = User.create!(user_data)
+    user.favorites.create!(location: "Denver, CO")
+    user.favorites.create!(location: "Dallas, TX")
+
+    user_api_key = {
+      "location": "Honolulu, HI",
+      "api_key": user.api_key
+    }
+
+    delete "/api/v1/favorites", params: user_api_key
+    expect(response.status).to eq(200)
     body = JSON.parse(response.body)
     expect(user.favorites.count).to eq(2)
 
