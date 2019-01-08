fd6ebccbb59dc5f5960e59b6e3c8250cedce1c11
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-08 10:04:45 -0700

    create Giphy Service to read url

---
+++ app/services/giphy_service.rb
@@ -0,0 +1,28 @@
+class GiphyService
+
+  def initialize(description)
+    @description = description
+  end
+
+  def gif
+    get_json["data"].first["url"]
+  end
+
+  private
+
+  def key
+    ENV['GIPHY_API']
+  end
+
+  def get_json
+    response = conn.get("?api_key=#{key}&q=#{@description}&limit=1&rating=G&lang=en")
+    JSON.parse(response.body)
+  end
+
+  def conn
+    Faraday.new(:url => 'https://api.giphy.com/v1/gifs/search') do |faraday|
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+
+end
