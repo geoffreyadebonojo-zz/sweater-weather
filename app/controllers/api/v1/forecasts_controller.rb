e998fd2750e4fab9aa0da90dbeca94557357bf66
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-09 05:35:29 -0700

    update

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::ForecastsController < ApplicationController
 
   def index
     location = params[:location] || "Denver, CO"
-    @data = GeocodeService.new(location).lat_lng
+    #@data = GeocodeService.new(location).lat_lng
+    @data = {"lat"=>39.7392358, "lng"=>-104.990251}
     render json: {data: ForecastsFacade.new.create_forecast(@data, location)}
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get '/', to: 'api/v1/forecasts#index'
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecasts#index'
