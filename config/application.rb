624be64105776a4b79d9c1c543a209ad4c0b513d
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-23 14:22:57 -0700

    reconfigure cors

--- config/application.rb
+++ config/application.rb
@@ -32,9 +32,5 @@ module SweaterWeather
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'http://my-web-service-consumer-site.com',
-      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-    }
   end
 end
